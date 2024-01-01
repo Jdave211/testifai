d68e8adf04289c3ff21df4f515ac673096ce9f56
Jdave211 <jagadave21@gmail.com>
2023-12-31 23:43:40 -0500

    apikey

--- src/services/article.js
+++ src/services/article.js
@@ -7,7 +7,7 @@ export const articleApi = createApi({
     baseQuery: fetchBaseQuery({
       baseUrl: 'https://article-extractor2.p.rapidapi.com/',
       prepareHeaders: (headers) => {
-        headers.set('X-RapidAPI-Key', '8e21c8a9a9msh3f67322cceb07a1p19d1e6jsnffcca359b7e5');
+        headers.set('X-RapidAPI-Key', rapidApiKey);
         headers.set('X-RapidAPI-Host', 'article-extractor2.p.rapidapi.com');
         return headers;
       },
