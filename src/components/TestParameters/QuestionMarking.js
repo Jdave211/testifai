296cae095b2a7a82cd2b15cab2bb0a3783ab9c53
Jdave211 <jagadave21@gmail.com>
2024-01-12 14:11:43 -0500

    Update QuestionMarking.js

--- src/components/TestParameters/QuestionMarking.js
+++ src/components/TestParameters/QuestionMarking.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function QuestionMarking({ userResponses, correctAnswers, selectedOptions }) {
     // Access the correct answers
