a0214045864a15eed2924b2d612c8644d6d79109
Jdave211 <jagadave21@gmail.com>
2024-01-17 22:03:14 -0500

    Update TestPage.js

--- src/components/TestPage/TestPage.js
+++ src/components/TestPage/TestPage.js
@@ -7,7 +7,8 @@ const TestPage = () => {
     const apiResponse = JSON.parse(window.localStorage.getItem('apiResponse'));
     const questions = apiResponse.questions.test.questions;
     const correctAnswers = apiResponse.answers;
-    const selectedOptions = JSON.parse(window.localStorage.getItem('selectedOptions'));
+    const selectedParams = JSON.parse(window.localStorage.getItem('selectedOptions'));
+    const quizType = selectedParams.quizType;
     const [selectedAnswers, setSelectedAnswers] = useState({});
     const [userResponses, setUserResponses] = useState({});
     const [isSubmitted, setIsSubmitted] = useState(false);
@@ -15,10 +16,10 @@ const TestPage = () => {
     const handleAnswerSelection = (questionIndex, selectedOption) => {
         let userResponse;
     
-        if (selectedOptions.quizType === 'multiple choice') {
+        if (quizType === 'multiple choice') {
             // Store the selected option index directly
             userResponse = selectedOption;
-        } else if (selectedOptions.quizType === 'true/false') {
+        } else if (quizType === 'true/false') {
             // Map 'true' or 'false' directly
             userResponse = selectedOption === 'True' ? 'True' : 'False';
         } else {
@@ -38,14 +39,14 @@ const TestPage = () => {
         questions.forEach((question, index) => {
             const questionIndex = index + 1; // 1-based index
     
-            if (selectedOptions.quizType === 'multiple choice' && selectedAnswers[questionIndex] !== undefined) {
+            if (quizType === 'multiple choice' && selectedAnswers[questionIndex] !== undefined) {
                 const alphabetOption = String.fromCharCode('a'.charCodeAt(0) + selectedAnswers[questionIndex]);
                 userResponses[questionIndex] = alphabetOption;
-            } else if (selectedOptions.quizType === 'true/false' && selectedAnswers[questionIndex] !== undefined) {
+            } else if (quizType === 'true/false' && selectedAnswers[questionIndex] !== undefined) {
                 userResponses[questionIndex] = selectedAnswers[questionIndex];
-            } else if (selectedOptions.quizType === 'short answer' && selectedAnswers[questionIndex] !== undefined) {
+            } else if (quizType === 'short answer' && selectedAnswers[questionIndex] !== undefined) {
                 userResponses[questionIndex] = selectedAnswers[questionIndex];
-            } else if (selectedOptions.quizType === 'essay' && selectedAnswers[questionIndex] !== undefined) {
+            } else if (quizType === 'essay' && selectedAnswers[questionIndex] !== undefined) {
                 userResponses[questionIndex] = selectedAnswers[questionIndex];
             }
         });
@@ -71,7 +72,7 @@ const TestPage = () => {
                 {questions.map((question, index) => (
                 <div key={index} className ='mb-7 mt-12'>
                     <p className='text-lg font-semibold'>{index + 1}. {question.question}</p>
-                    {selectedOptions.quizType === 'multiple choice' && question.options && typeof question.options === 'object' && Object.entries(question.options).map(([key, value], i) => (
+                    {quizType === 'multiple choice' && question.options && typeof question.options === 'object' && Object.entries(question.options).map(([key, value], i) => (
                     <div key={i} className='flex items-center'>
                         <input 
                             type='radio' 
@@ -83,20 +84,20 @@ const TestPage = () => {
                         <label htmlFor={`option${i}`} className='ml-2 mb-1'>{String.fromCharCode('A'.charCodeAt(0) + i)}: {value}</label>
                     </div>
                 ))}
-                    {selectedOptions.quizType === 'essay' && 
+                    {quizType === 'essay' && 
                         <textarea 
                             className='w-1/2 mt-4 rounded'
                             onChange={(e) => handleAnswerSelection(index, e.target.value)}
                         />
                     }
-                    {selectedOptions.quizType === 'short answer' && 
+                    {quizType === 'short answer' && 
                         <input 
                             type='text' 
                             className='w-1/3 mt-4 rounded'
                             onChange={(e) => handleAnswerSelection(index, e.target.value)}
                         />
                     }
-                    {selectedOptions.quizType === 'true/false' && question.options && typeof question.options === 'object' && Object.entries(question.options).map(([key, value], i) => (
+                    {quizType === 'true/false' && question.options && typeof question.options === 'object' && Object.entries(question.options).map(([key, value], i) => (
                         <div 
                             key={i} 
                             className='flex items-center'
@@ -111,7 +112,7 @@ const TestPage = () => {
             {isSubmitted && <QuestionMarking 
                                 userResponses={userResponses} 
                                 correctAnswers={correctAnswers} 
-                                selectedOptions={{ quizType: 'multiple choice' }} // Ensure this is defined
+                                quizType={quizType}
                             />}
             </div>
             <div className='text-right'>
