f218c0088604b18db93f00d1d8325861c6ee3aed
Jdave211 <jagadave21@gmail.com>
2024-01-23 23:38:13 -0500

    resultpage

---
+++ src/components/Feedback/ResultPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ResultPage = () => {
+    return (
+        <div>
+            <div>
+                <h1>Result Page</h1>
+            </div>
+        </div>
+    );
+}
+
+export default ResultPage;
