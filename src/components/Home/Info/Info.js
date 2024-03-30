6f6c991bb16835d64917205b09e86cb158ce0c3d
Jdave211 <jagadave21@gmail.com>
2024-03-30 11:34:55 -0400

    Update Info.js

--- src/components/Home/Info/Info.js
+++ src/components/Home/Info/Info.js
@@ -13,10 +13,10 @@ const Info = () => {
           </div>
           <div className='flex justify-between about'>
             <p className='column_text mr-5 font-bold mb-10'>
-              Your ultimate companion for interactive and personalized learning. As a cutting-edge quiz app, Testifai goes beyond traditional methods, offering a dynamic platform that generates detailed tests and quizzes based on your chosen knowledge base. Whether you're studying from lecture notes, videos, or textbooks, Testifai adapts to your content, providing a tailored quiz experience that reinforces your understanding.
+              Your ultimate companion for interactive and personalized learning. As a cutting-edge quiz webapp, Testifai goes beyond traditional methods, offering a dynamic platform that generates detailed tests and quizzes based on your chosen knowledge base. Whether you're studying from lecture notes, videos, or textbooks, Testifai adapts to your content, providing a tailored quiz experience that reinforces your understanding.
             </p>
             <p className='column_text ml-5 font-bold'>
-              Easily generate tests to practice and better understand stuff. Enter the URL of your class notes, video lecture, or document. Testifai will use this information to generate tailored quizzes for you. Select the difficulty level, test type (Multiple Choice, Short Answer, True/False, Essay, or Random), and the number of questions for your quiz. Click the "Generate Test" button to receive a detailed quiz based on your selected parameters.
+              Easily generate tests to practice and better understand stuff. Enter the URL of your class notes, video lecture, or document. Testifai will use this information to generate tailored quizzes for you. Select the difficulty level, test type (Multiple Choice, Short Answer, True/False, or Essay), and the number of questions for your quiz. Click the "Generate Test" button to receive a detailed quiz based on your selected parameters.
             </p>
           </div>
         </div>
