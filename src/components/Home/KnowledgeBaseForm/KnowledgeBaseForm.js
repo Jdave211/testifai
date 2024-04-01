87eee4320eab92fdde4194131b525a9865869d5b
Jdave211 <jagadave21@gmail.com>
2024-04-01 14:45:53 -0400

    gone

--- src/components/Home/KnowledgeBaseForm/KnowledgeBaseForm.js
+++ src/components/Home/KnowledgeBaseForm/KnowledgeBaseForm.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLazyGetArticlesQuery } from '../../../services/article.js';
-import { recognizeHandwriting } from '../../../services/handwrite.js';
 import { extractTextFromPDF } from './pdfUtils';
 import form from '../../images/form.png';
 
